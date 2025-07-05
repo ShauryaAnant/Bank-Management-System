64f4bb1438437319564a0fa82a526be7771cb5a9
Shaurya Anant <2023mcb1313@iitrpr.ac.in>
2025-07-05 14:01:50 +0530

    added .env and .env.production

--- bank-frontend/src/api/auth.js
+++ bank-frontend/src/api/auth.js
@@ -1,8 +1,9 @@
+// src/api/auth.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:9080';
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export async function login(username, password) {
   const response = await axios.post(`${API_BASE}/login`, { username, password });
   return response.data;
-} 
+}
--- bank-frontend/src/api/index.js
+++ bank-frontend/src/api/index.js
@@ -1,7 +1,8 @@
+// src/api/index.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9080',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-export default api; 
+export default api;
